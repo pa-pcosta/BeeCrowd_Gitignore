dcc53b498d248021a8531b1eb276dd056d5194dd
pa-pcosta <149397785+pa-pcosta@users.noreply.github.com>
2023-10-30 11:31:55 -0300

    Add files via upload

---
+++ BeeCrowd_C#_1036/URI_C#_1036/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Xml.Schema;
+
+namespace URI_C__1036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ler 3 variaveis double de uma string
+            string[] coeficientes = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(coeficientes[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(coeficientes[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(coeficientes[2], CultureInfo.InvariantCulture);
+            double delta = calculaDelta(a, b, c);
+
+            //Validar se é possível calcular as raizes
+                //a != 0 && delta >=0
+            if (!validaSeEhPossivelCalcularRaizes(a, delta))
+            {
+                Console.WriteLine("Impossivel calcular");
+
+                Console.ReadLine();
+
+                return;
+            }
+
+            //Calcular raízes
+
+            //Imprimir com 5 casas decimais
+
+            Console.WriteLine($"R1 = {calculaR1(a, b, delta):F5}");
+            Console.WriteLine($"R2 = {calculaR2(a, b,delta):F5}");
+
+            Console.ReadLine();
+        }
+
+        static bool validaSeEhPossivelCalcularRaizes(double a, double delta)
+        {
+            if ((a == 0) || (delta < 0)) 
+                { return false; }
+            else 
+                { return true; }
+        }
+
+        static double calculaDelta(double a, double b, double c)
+        {
+            return Math.Pow(b, 2) - 4 * a * c;
+        }
+
+        static double calculaR1(double a, double b, double delta)
+        {
+            return (-b + Math.Sqrt(delta)) / (2 * a);
+        }
+
+        static double calculaR2(double a, double b, double delta)
+        {
+            return (-b - Math.Sqrt(delta)) / (2 * a);
+        }
+    }
+}
---
+++ BeeCrowd_C#_1043/URI_C#_1043/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace URI_C__1043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ler 3 variaveis de uma string
+            string[] arestas = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(arestas[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(arestas[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(arestas[2], CultureInfo.InvariantCulture);
+
+            //Se formar triangulo calcular perimetro
+            if(VerificaSeFormaTriangulo(a, b, c))
+            {
+                Console.WriteLine($"Perimetro = {(a + b + c).ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+            //Se nao formar triangulo calcular area do trapezio BASE1=A, BASE2=B, ALTURA=C;
+            else
+            {
+                Console.WriteLine($"Area = {((a + b) * c /2).ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+            //Imprimir com 1 casa decimal
+
+            Console.ReadLine();
+        }
+
+        public static bool VerificaSeFormaTriangulo(double a, double b, double c)
+        {
+            return ((a < b+c) && (a > Math.Abs(b-c))) || ((b < a + c) && (b > Math.Abs(a - c))) || ((c < b + c) && (c > Math.Abs(b - a)));
+        }
+    }
+}
